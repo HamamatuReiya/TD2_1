3f4604f8c6966777288c080b21aa8770db9734b8
Oguro Haruka <Oguro Haruka@MSI>
2023-10-11 16:58:53 +0900

    上下移動

--- DirectXGame/RailCamera.h
+++ DirectXGame/RailCamera.h
@@ -1,2 +1,18 @@
-#pragma once
-class RailCamera {};
+﻿#pragma once
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+#include "Input.h"
+
+class RailCamera {
+public:
+	void Initialize(Vector3& worldPos, Vector3& rotation);
+	void Update();
+	ViewProjection& GetViewProjection() { return viewProjection_; }
+	WorldTransform& GetWorldTransform() { return worldTransform_; };
+
+private:
+	WorldTransform worldTransform_;
+	ViewProjection viewProjection_;
+	float fallY;
+	Input* input_ = nullptr;
+};
