5114d2d207d8f1816149fd82760fa04f113eb64a
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2023-10-19 11:35:22 +0900

    カメラの向き変更

--- DirectXGame/Item.cpp
+++ DirectXGame/Item.cpp
@@ -10,22 +10,18 @@ void Item::Initialize(Model* model, uint32_t textureHandle) {
 	worldTransform_.rotation_ = {0.0f, 0.0f, 0.0f};
 	worldTransform_.translation_ = {0.0f, 50.0f, 100.0f};
 	worldTransform_.Initialize();
-	worldTransform_.scale_ = {1.0f, 1.0f, 1.0f};
-	worldTransform_.rotation_ = {0.0f, 0.0f, 0.0f};
-	worldTransform_.translation_ = {0.0f, 0.0f, 0.0f};
 }
 void Item::Update() {
 
 }
-//void Item::Draw(ViewProjection& viewProjection) {
-//
-//}
-void Item::OnCollision() {
-
 void Item::Draw(ViewProjection& viewProjection) 
 {
 	model_->Draw(worldTransform_, viewProjection);
 }
+
+void Item::OnCollision() {}
+
+
 //Vector3 Item::GetWorldPosition() {
 //
 //}
